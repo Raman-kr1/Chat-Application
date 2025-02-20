b87abbae34e0db299c91e23f9c75e9070709e929
Raman <107814843+Raman-kr1@users.noreply.github.com>
2025-02-21 00:30:50 +0530

    Add files via upload

---
+++ frontend/src/components/join/Join.jsx
@@ -0,0 +1,29 @@
+import { useRef } from 'react';
+import io from 'socket.io-client';
+import style from './Join.module.css';
+import { Input, Button } from '@mui/material'
+
+export default function Join({ setChatVisibility, setSocket }) {
+
+    const usernameRef = useRef();
+
+    const handleSubmit = async () => {
+
+        const username = usernameRef.current.value;
+
+        if (!username.trim()) return
+
+        const socket = await io.connect('https://chat-realtime-nodejs.vercel.app/');
+        socket.emit('setUsername', username);
+        setSocket(socket);
+        setChatVisibility(true);
+    }
+
+    return (
+        <div className={style['join-container']}>
+            <h2>Chat em tempo real</h2>
+            <Input inputRef={usernameRef} placeholder='Nome de usuário' />
+            <Button sx={{ mt: 2 }} onClick={() => handleSubmit()} variant="contained">Entrar</Button>
+        </div>
+    );
+}
